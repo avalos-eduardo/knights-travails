ec7dd6dc7eb74f1d64207604314bebc1f8785b74
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2025-02-10 22:48:28 -0600

    knightMoves testing script

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { knightMoves } from "./knights-travails";
+
+console.log(knightMoves([0, 0], [1, 2]));
+console.log(knightMoves([0, 0], [3, 3]));
+console.log(knightMoves([3, 3], [0, 0]));
+console.log(knightMoves([0, 0], [7, 7]));
